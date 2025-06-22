48a1d5f386a99d0dd343aaa20745e658dd2bb791
Thales Alves Cirilo Batista <tacibatista@rd.com.br>
2025-06-22 10:13:57 -0300

    adicionado chassi do projeto spring 3 com java 17

---
+++ xp-desafio/src/main/java/br/com/xp/xp_desafio/XpDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.xp.xp_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpDesafioApplication.class, args);
+	}
+
+}
---
+++ xp-desafio/src/test/java/br/com/xp/xp_desafio/XpDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xp.xp_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
